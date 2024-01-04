71b38d8e0c568bdc20b957d584cc11d45b9f80e0
mCharatz <micharatz97@gmail.com>
2024-01-04 17:46:24 +0200

    fix top k baseline

--- scala-project/src/main/scala/TopKDominantPointsGlobal.scala
+++ scala-project/src/main/scala/TopKDominantPointsGlobal.scala
@@ -8,19 +8,19 @@ object TopKDominantPointsGlobal {
     val conf = new SparkConf().setAppName("Top K Dominant Points").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val data = sc.textFile("/home/michalis/IdeaProjects/DWS102-Spark-Project/datasets/4d_anticorrelated_data_10k.txt")
+    val data = sc.textFile("/home/michalis/IdeaProjects/DWS102-Spark-Project/datasets/3d_uniform_data.txt")
     val points = data.map(line => line.split("\\t").map(_.toDouble))
 
-    // Broadcast the entire dataset to each node
+    // broadcast the entire dataset to each node
     val broadcastedPoints = sc.broadcast(points.collect())
 
-    // Calculate the dominance score for each point
+    // calculate the dominance score for each point
     val scores = points.map { point =>
       val score = broadcastedPoints.value.count(otherPoint => isDominated(otherPoint, point))
       (point, score)
     }
 
-    // Find the top-k dominant points
+    // find the top-k dominant points
     val topK = scores.sortBy(-_._2).take(10)
 
     topK.foreach(point => println(point._1.mkString(", ") + " with score: " + point._2))
@@ -29,6 +29,10 @@ object TopKDominantPointsGlobal {
   }
 
   def isDominated(x: Array[Double], y: Array[Double]): Boolean = {
-    x.zip(y).forall { case (xi, yi) => xi <= yi } && x.zip(y).exists { case (xi, yi) => xi < yi }
+    x.zip(y).exists {
+      case (xi, yi) => xi > yi
+    } && !x.zip(y).exists {
+      case (xi, yi) => xi < yi
+    }
   }
 }
